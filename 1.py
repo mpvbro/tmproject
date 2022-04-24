103c9bc5f819ee2917a16bd21592048b5acec918
mpvbro <103290539+mpvbro@users.noreply.github.com>
2022-04-24 23:18:48 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="123", page_icon=":cyclone:", layout="wide")
+
+# HEADER SECTION
+st.subheader("hello, i am Max :wave:")
+st.title("project hopefully")
+st.write("123")
