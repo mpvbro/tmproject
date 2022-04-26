cfdc533e0aad8fbd086c1a557cb995b55ea81b93
mpvbro <103290539+mpvbro@users.noreply.github.com>
2022-04-26 14:55:23 +0500

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="123", page_icon=":cyclone:", layout="wide")
-
-# HEADER SECTION
-st.subheader("hello, i am Max :wave:")
-st.title("project hopefully")
-st.write("123")
